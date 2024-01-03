2b9b7ca3f65bd282f1f7f6e841b495b5daa7cd8b
gustavo-candidoo <lg0702@hotmail.com>
2024-01-03 09:55:50 -0300

    scrapping das noticias

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
+from bs4 import BeautifulSoup
+import requests
 
 # Lista de símbolos das ações
 acoes = ['WEGE3.SA', 'PETR4.SA', 'CEAB3.SA']
+
+# Mapeamento de tickers para termos de pesquisa
+ticker_mapping = {
+    'WEGE3.SA': 'weg',
+    'PETR4.SA': 'petrobras',
+    'CEAB3.SA': 'c&a'
+}
 
 # Período desejado (2023-01-01 a 2023-12-31)
 inicio = '2023-01-01'
@@ -21,25 +30,30 @@ app = dash.Dash(__name__)
 # Layout do dashboard
 app.layout = html.Div([
     html.H1("Dashboard de Dados Históricos de Ações"),
-    
+
     dcc.Dropdown(
         id='dropdown-acao',
         options=[{'label': acao, 'value': acao} for acao in acoes],
         value='WEGE3.SA',
         multi=False
     ),
-    
+
     dcc.Graph(
         id='graph-dados-historicos'
-    )
+    ),
+
+    html.Div(id='news-content')
 ])
 
-# Callback para atualizar o gráfico com base na seleção do usuário
+# ...
+
+# Callback para atualizar o gráfico e obter notícias com base na seleção do usuário
 @app.callback(
-    Output('graph-dados-historicos', 'figure'),
+    [Output('graph-dados-historicos', 'figure'),
+     Output('news-content', 'children')],
     [Input('dropdown-acao', 'value')]
 )
-def update_graph(selected_acao):
+def update_graph_and_news(selected_acao):
     # Criação do gráfico de velas com Plotly
     fig = go.Figure(data=[go.Candlestick(x=dados_acoes[selected_acao].index,
                                          open=dados_acoes[selected_acao]['Open'],
@@ -51,68 +65,38 @@ def update_graph(selected_acao):
                       xaxis_title='Data',
                       yaxis_title='Preço',
                       xaxis_rangeslider_visible=False)
-    
-    return fig
 
-# Execução do aplicativo
-if __name__ == '__main__':
-    app.run_server(debug=True)
-import yfinance as yf
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.graph_objects as go
+    # Scraping de notícias com base no ticker
+    search_term = ticker_mapping[selected_acao]
+    search_url = f'https://braziljournal.com/?s={search_term}'
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# Lista de símbolos das ações
-acoes = ['WEGE3.SA', 'PETR4.SA', 'CEAB3.SA']
+    # Encontrar os elementos <li> dentro da lista
+    news_items = soup.select('ul#allnews > li')
 
-# Período desejado (2023-01-01 a 2023-12-31)
-inicio = '2023-01-01'
-fim = '2023-12-31'
+    # Inicializar listas para armazenar informações extraídas
+    news_links = []
+    news_titles = []
+    news_descriptions = []
 
-# Dados históricos para cada ação
-dados_acoes = {acao: yf.download(acao, start=inicio, end=fim) for acao in acoes}
+    # Limitar a 3 notícias
+    for i, news_item in enumerate(news_items):
+        if i >= 3:
+            break
 
-# Inicialização do aplicativo Dash
-app = dash.Dash(__name__)
+        link = news_item.select_one('h2 a')['href']
+        title = news_item.select_one('.boxarticle-infos-title a').text
+        description = news_item.select_one('.boxarticle-infos-text').text.strip()
 
-# Layout do dashboard
-app.layout = html.Div([
-    html.H1("Dashboard de Dados Históricos de Ações"),
-    
-    dcc.Dropdown(
-        id='dropdown-acao',
-        options=[{'label': acao, 'value': acao} for acao in acoes],
-        value='WEGE3.SA',
-        multi=False
-    ),
-    
-    dcc.Graph(
-        id='graph-dados-historicos'
-    )
-])
+        news_links.append(link)
+        news_titles.append(title)
+        news_descriptions.append(description)
 
-# Callback para atualizar o gráfico com base na seleção do usuário
-@app.callback(
-    Output('graph-dados-historicos', 'figure'),
-    [Input('dropdown-acao', 'value')]
-)
-def update_graph(selected_acao):
-    # Criação do gráfico de velas com Plotly
-    fig = go.Figure(data=[go.Candlestick(x=dados_acoes[selected_acao].index,
-                                         open=dados_acoes[selected_acao]['Open'],
-                                         high=dados_acoes[selected_acao]['High'],
-                                         low=dados_acoes[selected_acao]['Low'],
-                                         close=dados_acoes[selected_acao]['Close'])])
+    # Retornar o gráfico de velas e o conteúdo das notícias
+    return fig, [html.Div([html.H3(title), html.P(description)]) for title, description in zip(news_titles, news_descriptions)]
 
-    fig.update_layout(title=f'Gráfico de Velas - {selected_acao}',
-                      xaxis_title='Data',
-                      yaxis_title='Preço',
-                      xaxis_rangeslider_visible=False)
-    
-    return fig
+# ...
 
-# Execução do aplicativo
 if __name__ == '__main__':
     app.run_server(debug=True)
