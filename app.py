48ac35fe96e996c7851e683fdd157f14e293308b
gustavo-candidoo <lg0702@hotmail.com>
2024-01-04 09:49:06 -0300

    ticker c&a e remocação de comentários

--- app.py
+++ app.py
@@ -6,40 +6,33 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 from bs4 import BeautifulSoup
 import requests
+import datetime as dt
 
-# Lista de símbolos das ações
 acoes = ['WEGE3.SA', 'PETR4.SA', 'CEAB3.SA']
 
-# Mapeamento de tickers para termos de pesquisa
 ticker_mapping = {
     'WEGE3.SA': 'weg',
     'PETR4.SA': 'petrobras',
-    'CEAB3.SA': 'c&a'
+    'CEAB3.SA': 'c%26a'
 }
 
-# Período desejado (2023-01-01 a 2023-12-31)
 inicio = '2023-01-01'
-fim = '2023-12-31'
+fim = dt.datetime.now()
 
-# Dados históricos para cada ação
 dados_acoes = {acao: yf.download(acao, start=inicio, end=fim) for acao in acoes}
 
-# Inicialização do aplicativo Dash
 app = dash.Dash(__name__)
 
-# Layout do dashboard
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content', className='container-fluid', style={'backgroundColor': 'black'})  # Adiciona estilo de fundo preto
+    html.Div(id='page-content', className='container-fluid', style={'backgroundColor': 'black'}) 
 ])
 
-# Página inicial
 index_page = html.Div([
     dcc.Markdown('''
     # Dashboard de Dados Históricos de Ações
     '''),
     html.Div([
-        # Layout da esquerda (gráfico e dropdown)
         html.Div([
             dcc.Dropdown(
                 id='dropdown-acao',
@@ -50,26 +43,23 @@ index_page = html.Div([
             ),
             dcc.Graph(
                 id='graph-dados-historicos',
-                style={'width': '100%', 'height': '70vh'}  # Ajusta a largura e altura do gráfico
+                style={'width': '100%', 'height': '70vh'} 
             ),
         ], className="col-md-6"),
 
-        # Layout da direita (notícias)
         html.Div([
-            html.H2("Notícias", className="mb-3", style={'color': 'white', 'font-weight': 'bold'}),  # Ajusta o estilo do título
+            html.H2("Notícias", className="mb-3", style={'color': 'white', 'font-weight': 'bold'}), 
             html.Div(id='news-content')
         ], className="col-md-6"),
     ], className="row"),
 ])
 
-# Callback para atualizar o gráfico e obter notícias com base na seleção do usuário
 @app.callback(
     [Output('graph-dados-historicos', 'figure'),
      Output('news-content', 'children')],
     [Input('dropdown-acao', 'value')]
 )
 def update_graph_and_news(selected_acao):
-    # Criação do gráfico de velas com Plotly
     fig = go.Figure(data=[go.Candlestick(x=dados_acoes[selected_acao].index,
                                          open=dados_acoes[selected_acao]['Open'],
                                          high=dados_acoes[selected_acao]['High'],
@@ -81,21 +71,17 @@ def update_graph_and_news(selected_acao):
                       yaxis_title='Preço',
                       xaxis_rangeslider_visible=False)
 
-    # Scraping de notícias com base no ticker
     search_term = ticker_mapping[selected_acao]
     search_url = f'https://braziljournal.com/?s={search_term}'
     response = requests.get(search_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Encontrar os elementos <li> dentro da lista
     news_items = soup.select('ul#allnews > li')
 
-    # Inicializar listas para armazenar informações extraídas
     news_links = []
     news_titles = []
     news_descriptions = []
 
-    # Limitar a 3 notícias
     for i, news_item in enumerate(news_items):
         if i >= 3:
             break
@@ -108,15 +94,11 @@ def update_graph_and_news(selected_acao):
         news_titles.append(title)
         news_descriptions.append(description)
 
-    # Retornar o gráfico de velas e o conteúdo das notícias
     return fig, [html.Div([
-        html.H3(html.A(title, href=link, target='_blank')),  # Adiciona o link no título
+        html.H3(html.A(title, href=link, target='_blank')),  
         html.P(description),
     ]) for title, description, link in zip(news_titles, news_descriptions, news_links)]
 
-# ...
-
-# Atualização do layout baseado na URL
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
